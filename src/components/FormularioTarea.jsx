2912fb2553541d1e2f69dc8dec94235657a3cd49
Juan Gerardo Romero Uro <jgromerouro@gmail.com>
2023-05-23 13:07:19 -0300

    se usó el useState para el valor input del formulario y se usó onSubmit para el input tarea a la función handleSubmit

--- src/components/FormularioTarea.jsx
+++ src/components/FormularioTarea.jsx
@@ -1,12 +1,27 @@
-import { Form, Button } from "react-bootstrap";
-import ListaTareas from "./ListaTareas";
+import { useState } from 'react';
+import { Form, Button } from 'react-bootstrap';
+import ListaTareas from './ListaTareas';
 
 const FormularioTarea = () => {
+  const [inputValue, setInputValue] = useState('');
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(inputValue);
+  };
+
+  //Todo: Hacer la logica de agregar una tarea a la lista
+
   return (
     <section>
-      <Form>
+      <Form onSubmit={handleSubmit}>
         <Form.Group className="mb-3 d-flex" controlId="tarea">
-          <Form.Control type="text" placeholder="Ingrese una tarea" />
+          <Form.Control
+            type="text"
+            placeholder="Ingrese una tarea"
+            value={inputValue}
+            onChange={(event) => setInputValue(event.target.value)}
+          />
           <Button variant="primary" type="submit">
             Enviar
           </Button>
